04282012e54799647b203860b219d6afd86ffc37
rencelibrando <clarencemanlolo@gmail.com>
2026-09-16 17:00:14 +0800

    clean the files and remove test files

--- service-rag-python/test_db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect("postgresql://postgres:postgres@127.0.0.1:54322/postgres")
-cur = conn.cursor()
-cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public';")
-tables = cur.fetchall()
-print("Tables in public schema:")
-for t in tables:
-    print("-", t[0])
-conn.close()
